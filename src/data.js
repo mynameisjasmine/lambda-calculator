30e897b9b675048c498fb6f1711b8faca6f45b20
Jasmine <jasminecharles@hotmail.com>
2019-07-16 12:26:03 -0700

    test

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-// STEP 1 - Get the modules here exported before building out your components
+// STEP 1 - Get the modules, here exported before building out your components
 // Don't worry about bringing all of these into the same file.
 // Export them as necessary and import each array into its appropriate
 // file. No real tricks here just be aware of what is in each array
