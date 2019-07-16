c547e5a8ec2f156bbb9c88e0c85b892fc2ecf0df
Jasmine <jasminecharles@hotmail.com>
2019-07-16 14:28:37 -0700

    exported arrays

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
-// STEP 1 - Get the modules, here exported before building out your components
+// STEP 1 - Get the modules here exported before building out your components
 // Don't worry about bringing all of these into the same file.
 // Export them as necessary and import each array into its appropriate
 // file. No real tricks here just be aware of what is in each array
 // and how you'll access the data.
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+ const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
-const operators = [
+ const operators = [
   {
     char: "/",
     value: "/"
@@ -29,4 +29,5 @@ const operators = [
   }
 ];
 
-const specials = ["C", "+/-", "%"];
+  const specials = ["C", "+/-", "%"];
+ export {numbers, operators, specials}
