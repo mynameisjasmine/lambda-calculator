8c6be3a865b0398afaa007ee2e09b1eb12649028
Jasmine <jasminecharles@hotmail.com>
2019-07-16 21:20:42 -0700

    display test

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { operators } from "../../../data.js";
-i
+import OperatorButton from "./OperatorButton"
 //import any components needed
 
 //Import your array data to from the provided data file
@@ -16,6 +16,8 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {operatorState.map((op, index) => (<OperatorButton char = {op.char} value= {op.value} key = {index}/>
+    ))}
     </div>
   );
 };
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return <div>{/* Display any props data here */}display</div>;
 };
+
+export default Display
