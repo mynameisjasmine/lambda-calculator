d23079174d45a634d220457c547204afe07d9081
Jasmine <jasminecharles@hotmail.com>
2019-09-10 13:23:07 -0700

    test

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const OperatorButton = (props) => {
+const OperatorButton = ({char, value}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     
-      <button>{props.index}</button>
+      <button>{char}</button>
     </>
   );
 };
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}display</div>;
+  return <div>{/* Display any props data here */}</div>;
 };
 
 export default Display
